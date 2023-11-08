774429fb24e9c66bb9e80d25cba6a1894cfd5b89
NikitaKST <8172535@gmail.com>
2023-11-08 19:00:28 +0500

    add code createRequest.js

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -1,7 +1,31 @@
-/**
- * Основная функция для совершения запросов
- * на сервер.
- * */
 const createRequest = (options = {}) => {
+  const xhr = new XMLHttpRequest;
+  const formData = new FormData();
+  xhr.responseType = 'json';
 
+  if(options.method == 'GET') {
+      options.url += '?';
+      for(let key in options.data) {
+          options.url += `${key}=${options.data[key]}&`;
+      }
+  }
+  
+  if(options.method != 'GET') {
+      for(let key in options.data) {
+          formData.append(key, options.data[key]);
+      }
+  }
+  
+  try {
+      xhr.open (options.method, options.url);
+      xhr.send(formData);
+  } catch (e) {
+      console.log('catch' + e);
+  };
+  xhr.onload = () => {
+      options.callback(null, xhr.response);
+  };
+  xhr.onerror = () => { 
+      options.callback(xhr.statusText, null);
+  };
 };
