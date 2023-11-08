89288206425a02e92154094663973acd194882fa
NikitaKST <8172535@gmail.com>
2023-11-08 19:12:43 +0500

    add code modal.js

--- public/js/ui/Modal.js
+++ public/js/ui/Modal.js
@@ -12,8 +12,13 @@ class Modal {
    * необходимо выкинуть ошибку.
    * */
   constructor(element){
-
-  }
+    if (!element) {
+      throw new Error(`Error empty ${element} in class Modal`);
+    } else {
+        this.element = element;
+        this.registerEvents();
+    };
+  };
 
   /**
    * При нажатии на элемент с data-dismiss="modal"
@@ -21,27 +26,31 @@ class Modal {
    * (с помощью метода Modal.onClose)
    * */
   registerEvents() {
-
-  }
+    const btnClose = this.element.querySelectorAll('button[data-dismiss="modal"]');
+    btnClose.forEach(elem => elem.addEventListener('click', (e) => {
+        this.onClose(e);
+    }));
+  };
 
   /**
    * Срабатывает после нажатия на элементы, закрывающие окно.
    * Закрывает текущее окно (Modal.close())
    * */
   onClose(e) {
-
+    e.preventDefault();
+    this.close();
   }
   /**
    * Открывает окно: устанавливает CSS-свойство display
    * со значением «block»
    * */
   open() {
-
+    this.element.style.display = 'block';
   }
   /**
    * Закрывает окно: удаляет CSS-свойство display
    * */
   close(){
-
+    this.element.style.display = 'none';
   }
 }
